263917b4e1f30ff851f38c5d4e1f2be0ff55216d
Ethiogit9 <e553c5@gmail.com>
2025-03-15 11:00:35 -0400

    commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // local version
   // baseURL: "http://127.0.0.1:5001/clone-fb0c9/us-central1/api",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://amazon-backend-klfp.onrender.com/",
 
   // deployed version
   //   baseURL: "https://amazon-backend-4kfd.onrender.com/",
